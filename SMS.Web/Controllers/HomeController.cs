1131622275fa2cf996746317e2a4f5648147fe41
Stephen <denial1997@hotmail.co.uk>
2022-02-16 12:39:52 +0000

    Added About Page

--- SMS.Web/Controllers/HomeController.cs
+++ SMS.Web/Controllers/HomeController.cs
@@ -15,12 +15,34 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        var longDate = DateTime.Now.ToLongDateString();
+        var longTime = DateTime.Now.ToLongTimeString();
+
+        ViewBag.LongDate = longDate;
+        ViewBag.LongTime = longTime;
+
         return View();
     }
 
     public IActionResult Privacy()
     {
         return View();
+    }
+
+    public IActionResult About()
+    {
+        var title = "This is a title";
+        var message = "This is a message";
+        var company = "Bond Technologies";
+
+        var about = new AboutViewModel {
+            Formed = new DateTime(2022, 6, 25),
+            Title = title,
+            Message = message,
+            Company = company
+        };
+
+        return View(about);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ SMS.Web/Models/AboutViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+namespace SMS.Web.Models
+{
+    public class AboutViewModel
+    {
+        public string Title { get; set; }
+        public string Company { get; set; }        
+        public string Message { get; set; }
+        public DateTime Formed { get; set; } = DateTime.Now;
+        public string FormedString => Formed.ToLongDateString();
+        public int Days => (DateTime.Now - Formed).Days;
+    }
+}
